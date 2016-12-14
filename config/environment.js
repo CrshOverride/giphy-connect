0f905351704f082b72eddd365a00d41f817a8a16
Justin Niessner <CrshOverride@gmail.com>
2016-12-13 20:26:04 -0500

    Stop overriding Date.parse

--- config/environment.js
+++ config/environment.js
@@ -11,13 +11,16 @@ module.exports = function(environment) {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
+      },
+      EXTEND_PROTOTYPES: {
+        Date: false
       }
     },
 
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-    }
+    },
   };
 
   if (environment === 'development') {
